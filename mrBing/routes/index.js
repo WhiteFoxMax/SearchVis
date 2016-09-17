0e080a146c806ecbe490d43c1797310bd14e6721
MaxBk <maxipoo@gmail.com>
2016-09-18 01:01:49 +0300

    BabyStept

--- mrBing/routes/index.js
+++ mrBing/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var MS_AccountKey = "";
+var bing_API_url =  "https://api.datamarket.azure.com/Bing/SearchWeb";
+
+var auth = {
+  user: MS_AccountKey,
+  pass: MS_AccountKey
+};
+
+var qs = {
+  "$format" : "json",
+  Query: "'" + "Mustang" + "'"
+};//Change Mustang to query later
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request.get(bing_API_url, {
+    'auth': auth,
+    'qs': qs
+  }, function(error, response, body){
+    console.log("~!~!@~!@~! Bing bing bing ~!@~!@~!@~!@");
+    res.json(body);
+  });
 });
 
 module.exports = router;
